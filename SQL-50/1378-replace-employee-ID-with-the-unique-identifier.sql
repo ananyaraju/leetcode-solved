9ef96ac18282bd32e0b58f1180d5716f8a1d2613
Ananya <ananyaraj24@gmail.com>
2023-05-24 17:01:50 +0530

    24/05

---
+++ SQL-50/1378-replace-employee-ID-with-the-unique-identifier.sql
@@ -0,0 +1,1 @@
+SELECT euni.unique_id, e.name FROM employees e LEFT JOIN employeeUNI euni ON e.id=euni.id;
---
+++ SQL-50/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,1 @@
+SELECT v.customer_id, count(v.visit_id) AS count_no_trans FROM visits v LEFT JOIN transactions t ON v.visit_id=t.visit_id WHERE t.transaction_id IS NULL GROUP BY v.customer_id;
