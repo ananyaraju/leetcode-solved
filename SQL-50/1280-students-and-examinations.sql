4ceaeaef1597c2bdb5db32fb1b6a1c4774560094
Ananya <ananyaraj24@gmail.com>
2023-05-25 12:09:38 +0530

    25/05

---
+++ SQL-50/1280-students-and-examinations.sql
@@ -0,0 +1,1 @@
+SELECT stu.student_id, stu.student_name, sub.subject_name, count(e.subject_name) AS attended_exams FROM students stu CROSS JOIN subjects sub LEFT JOIN examinations e ON stu.student_id=e.student_id AND e.subject_name=sub.subject_name GROUP BY stu.student_id, sub.subject_name ORDER BY stu.student_id, sub.subject_name;
