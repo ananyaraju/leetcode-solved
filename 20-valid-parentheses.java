7836b0bb6bfd6a206f8881bb6db88df228db300f
Ananya <ananyaraj24@gmail.com>
2023-05-26 10:41:35 +0530

    26/05

---
+++ 20-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.println(c);
+            if (c=='{' || c=='[' || c=='(') {
+                st.push(c);
+                continue;
+            }
+            if (c=='}' && !st.isEmpty() && st.peek()=='{') {
+                st.pop();
+                continue;
+            }
+            if (c==']' && !st.isEmpty() && st.peek()=='[') {
+                st.pop();
+                continue;
+            }
+            if (c==')' && !st.isEmpty() && st.peek()=='(') {
+                st.pop();
+                continue;
+            }
+            if (c=='}' || c==']' || c==')') {
+                st.push(c);
+                break;
+            }
+        }
+        return st.isEmpty();
+    }
+}
