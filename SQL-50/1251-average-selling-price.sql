5db521ac5106538666c74f722773499dc3422703
Ananya <ananyaraj24@gmail.com>
2023-05-26 12:09:43 +0530

    26/05

---
+++ SQL-50/1251-average-selling-price.sql
@@ -0,0 +1,1 @@
+SELECT p.product_id, round(sum(p.price*u.units)/sum(u.units),2) AS average_price FROM prices p JOIN unitsSold u ON p.product_id=u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date GROUP BY p.product_id;
