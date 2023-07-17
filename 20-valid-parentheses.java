b5319162f8c670c288726084640f48b34228a6cb
Ananya <ananyaraj24@gmail.com>
2023-07-18 00:03:05 +0530

    17/07

--- 20-valid-parentheses.java
+++ 20-valid-parentheses.java
@@ -3,7 +3,6 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for (int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
-            System.out.println(c);
             if (c=='{' || c=='[' || c=='(') {
                 st.push(c);
                 continue;
