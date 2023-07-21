a2321f0db48c56cbda4ffde42a3403d67c492872
Ananya <ananyaraj24@gmail.com>
2023-07-21 22:02:06 +0530

    21/07

---
+++ 1528-shuffle-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char shuffle[] = new char[indices.length];
+        for (int i=0; i<indices.length; i++)
+            shuffle[indices[i]] = s.charAt(i);
+        return String.valueOf(shuffle);
+    }
+}
