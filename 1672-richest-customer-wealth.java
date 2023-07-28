d9129ebb7f2ed6434b01627dd05a031fa01f5a72
Ananya <ananyaraj24@gmail.com>
2023-07-28 13:26:29 +0530

    28/07

--- 1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth.java
@@ -13,3 +13,18 @@ class Solution {
         return wealth[wealth.length-1];
     }
 }
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int sum = 0;
+        for (int i=0; i<accounts.length; i++) {
+            for (int j=0; j<accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(max, sum);
+            sum = 0;
+        }
+        return max;
+    }
+}
